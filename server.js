e551ff6df5aad791714dee901dc7c79e1978c2a6
Abhilash SB <abhilashsb9@gmail.com>
2018-03-14 23:25:05 +0530

    Save santhi blog data and get all santhi blog data APIs

--- server.js
+++ server.js
@@ -49,6 +49,21 @@ app.post('/api/yogablog/save', function(req, res) {
     service.saveYogaBlog(req.body);
     res.status(200).send("The blog has been saved successfully!!");
 });
+
+app.post('/api/santhiblog/save', function(req, res) {
+    service.saveSanthiBlog(req.body);
+    res.status(200).send("The blog has been saved successfully!!");
+});
+
+app.post('/api/blog/delete', function(req, res) {
+    service.deleteBlogs(req.body);
+    res.status(200).send("The event has been deleted successfully!!");
+});
+app.post('/api/santhiblog/delete', function(req, res) {
+    service.deleteSanthiBlogs(req.body);
+    res.status(200).send("The event has been deleted successfully!!");
+});
+
 app.post('/api/blog/save', function(req, res) {
     service.saveBlog(req.body);
     res.status(200).send("The blog has been saved successfully!!");
@@ -151,6 +166,14 @@ app.get('/login', function(req, res) {
 app.get('/api/blog/getAllBlogs', function(req, res) {
     var events;
     service.readAllBlogs().then((result) => {
+        res.send(result);
+    }).catch(function(error) {
+        res.status(500).send(error);
+    });
+});
+app.get('/api/santhiblog/getAllBlogs', function(req, res) {
+    var events;
+    service.getAllSanthBlogs().then((result) => {
         res.send(result);
     }).catch(function(error) {
         res.status(500).send(error);
